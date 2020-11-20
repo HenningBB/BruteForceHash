54df301f3bf68c43094f3e7320b1f5cd8d5b90f7
HenningBB <henning.bull-berg@tsbw.cloud>
2020-11-20 13:03:13 +0100

    kinda functionally

--- BruteForceHash/MainWindow.xaml.cs
+++ BruteForceHash/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ namespace BruteForceHash
         string pruf = "1";
         string ausgabe = "";
         string ausgabeHash = "";
+        bool broken = false;
+        string eingabe2="";
 
 
         public MainWindow()
@@ -29,42 +31,129 @@ namespace BruteForceHash
             }
             else
             {
-                breakHash(eingabe);
+                txtAusgabe.Text = "Berechne!";
+                ausgabe = "";
+                broken = false;
+                rekursion(cmbLength.SelectedIndex+1, 1);
             }
         }
 
-        private void breakHash(string angabe)
+        private void rekursion(int length, int level)
         {
-            for (int i = 0; i < 62; i++)
+            if (!broken)
             {
-                ausgabe = intToChar(i).ToString();
-                ausgabeHash = hashAString(ausgabe);
-                if (ausgabeHash == eingabe)
+                if (length == level)
                 {
-                    txtAusgabe.Text = ausgabe;
+                    breakHash();
+                }
+                else
+                {
+                    for (int i = 0; i < 62; i++)
+                    {
+                        string angabe = intToString(i);
+
+                        if (level == 1)
+                        {
+                            ausgabe = angabe;
+                        }
+                        else
+                        {
+                            if (ausgabe.Length < level)
+                            {
+                                ausgabe += angabe;
+                            }
+                            else
+                            {
+                                char[] ergabe = ausgabe.ToCharArray();
+                                ergabe[level - 1] = intToChar(i);
+                                ausgabe = "";
+                                foreach (var item in ergabe)
+                                {
+                                    if (ausgabe.Length == 0)
+                                    {
+                                        ausgabe = item.ToString();
+                                    }
+                                    else
+                                    {
+                                        ausgabe += item.ToString();
+                                    }
+                                }
+                            }
+                        }
+                        rekursion(length, level + 1);
+                    }
                 }
             }
         }
 
-        private char intToChar(int increment)
+
+
+        private void breakHash()
+        {
+            for (int i = 0; i < 62; i++)
+            {
+                string angabe = ausgabe;
+                angabe += intToString(i);
+                ausgabeHash = hashAString(angabe);
+                if (ausgabeHash == eingabe)
+                {
+                    txtAusgabe.Text = angabe;
+                    broken = true;
+                    break;
+                }
+                else
+                {
+                    txtAusgabe.Text = "Kein Ergebnis!";
+                }
+            }
+        }
+
+        private string intToString(int increment)
         {
             char c;
+            //Zahlen unicodedezimalstelle von 48-57, increment von 0-9
             if (increment < 10)
             {
                 increment += 48;
                 c = (char)increment;
             }
+            //kleinbuchstaben unicodedezimalstelle von 97-122, increment von 10-35
             else if (increment < 36)
             {
                 increment += 87;
                 c = (char)increment;
             }
+            //großbuchstaben unicodedzimalstelle von 65-90, increment von 36-62
             else
             {
                 increment += 29;
                 c = (char)increment;
             }
-            return c;            
+            return c.ToString();
+        }
+
+        private char intToChar(int increment)
+        {
+            char c;
+            //Zahlen unicodedezimalstelle von 48-57, increment von 0-9
+            if (increment < 10)
+            {
+                increment += 48;
+                c = (char)increment;
+            }
+            //kleinbuchstaben unicodedezimalstelle von 97-122, increment von 10-35
+            else if (increment < 36)
+            {
+                increment += 87;
+                c = (char)increment;
+            }
+            //großbuchstaben unicodedzimalstelle von 65-90, increment von 36-62
+            else
+            {
+                increment += 29;
+                c = (char)increment;
+            }
+            return c;
         }
 
         #region Warnsholtscher Hashingprozess
@@ -88,5 +177,12 @@ namespace BruteForceHash
             return result.ToString();
         }
         #endregion
+
+        private void btnBrechenTest_Click(object sender, RoutedEventArgs e)
+        {
+            eingabe2 = txtHashEingabeTest.Text;
+            eingabe2 = hashAString(eingabe2);
+            txtAusgabe2.Text = eingabe2;
+        }
     }
 }
